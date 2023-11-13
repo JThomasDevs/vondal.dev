72b4fe2010804267486a2b0acd94d43df077fd6c
Jonathan Thomas <jthomasahs14@gmail.com>
2023-11-12 19:52:46 -0700

    some comments

--- website/src/Routing.js
+++ website/src/Routing.js
@@ -20,9 +20,11 @@ export default function Routing() {
                 <Route path="/cpp" element={<Construction header={
                     <h1 className="text-cpp text-header-text font-semibold flex justify-center pt-6">{'{'}&nbsp;<a href="https://cplusplus.com" target="_blank" rel="noreferrer" className="hover:underline">C++</a>&nbsp;{'}'}</h1>
                 } /> } />
+                { /* Rust blog page */ }
                 <Route path="/rust" element={<Construction header={
                     <h1 className="text-rust text-header-text font-semibold flex justify-center pt-6">{'{'}&nbsp;<a href="https://www.rust-lang.org/" target="_blank" rel="noreferrer" className="hover:underline">Rust</a>&nbsp;{'}'}</h1>
                 } />} />
+                { /* Java blog page */ }
                 <Route path="/java" element={<Construction header={
                     <h1 className="text-java text-header-text font-semibold flex justify-center pt-6">{'{'}&nbsp;<a href="https://dev.java/" target="_blank" rel="noreferrer" className="hover:underline">Java</a>&nbsp;{'}'}</h1>
                 } />} />
