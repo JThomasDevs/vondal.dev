2222db9bd4078a80a3e0322e08e3fc2c2cc6d211
JThomasDevs <jthomasahs14@gmail.com>
2023-12-18 00:35:39 -0700

    formatting changes

--- website/src/pages/Construction.js
+++ website/src/pages/Construction.js
@@ -12,7 +12,7 @@ function Construction({ header }) {
             { header }
             <HomeButton />
             <div className="bg-black font-mono text-center flex-col items-center justify-center">
-                <p className="text-content-text tracking-tighter leading-6 flex-col text-center pl-10vw pr-10vw pt-4">
+                <p className="text-content-text tracking-tighter leading-6 flex-col text-center body-text pt-4">
                     This Page is Under Construction
                 </p>
                 <BusinessContact />
--- website/src/pages/HomePage.js
+++ website/src/pages/HomePage.js
@@ -3,13 +3,13 @@ import BusinessContact from "../components/BusinessContact";
 export default function HomePage() {
     return (
         <div className="bg-black font-mono text-center flex-col items-center justify-center scale-100">
-            <h1 className="text-header-text font-semibold pt-6 pb-2">
+            <h1 className="text-header-text font-semibold pt-8 pb-2">
                 {'{ Vondal }'}
             </h1>
-            <h2 className="text-content-text tracking-tighter leading-6 flex-col text-center pl-10vw pr-10vw pt-0 pb-8">
+            <h2 className="text-content-text tracking-tighter leading-6 flex-col text-center body-text pt-20 pb-8">
                 I am a 27-year-old full stack software engineer currently earning my degree in CS.
             </h2>
-            <p className="text-content-text tracking-tighter leading-6 flex-col text-center pl-10vw pr-10vw pt-8 pb-4">
+            <p className="text-content-text tracking-tighter leading-6 flex-col text-center body-text pt-8 pb-4">
                 <div className="pb-6">
                     This website was created using <a target="_blank" rel="noreferrer" href="https://react.dev/" className="text-react hover:underline">React</a>, <a target="_blank" rel="noreferrer" href="https://tailwindcss.com/" className="text-tailwind hover:underline">TailwindCSS</a>, and <a target="_blank" rel="noreferrer" href="https://firebase.google.com/" className="text-firebase hover:underline">Firebase</a>.
                 </div>
