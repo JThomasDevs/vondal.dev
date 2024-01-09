9dd3a163070ece508722687aa1a4d77196002a19
Jonathan Thomas <jthomasahs14@gmail.com>
2024-01-08 23:27:27 -0700

    Active age
    Added react hooks to actively update my age every 0.1 seconds. Age is shown to 10 decimal places.

--- website/src/pages/HomePage.js
+++ website/src/pages/HomePage.js
@@ -1,13 +1,34 @@
+import React from "react";
 import BusinessContact from "../components/BusinessContact";
+import { useState, useEffect } from "react";
 
 export default function HomePage() {
+    const birthDate = new Date('1995-12-09T15:00:00-06:00');
+
+    const [age, setAge] = useState(calculateAge(birthDate));
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setAge(calculateAge(birthDate));
+        }, 100); // Update every tenth of a second
+
+        return () => clearInterval(interval);
+    }, []);
+
+    function calculateAge() {
+        const currentDate = new Date();
+        const ageInMilliseconds = currentDate - birthDate;
+        const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+        return ageInYears.toFixed(10);
+    }
+
     return (
         <div className="bg-black font-mono text-center flex-col items-center justify-center scale-100">
             <h1 className="text-header-text font-semibold pt-8 pb-2">
                 {'{ Vondal }'}
             </h1>
             <h2 className="text-content-text tracking-tighter leading-6 flex-col text-center body-text pt-20 pb-8">
-                I am a 27-year-old full stack software engineer currently earning my degree in CS.
+                I am a {age} year old full stack software engineer currently earning my degree in CS.
             </h2>
             <p className="text-content-text tracking-tighter leading-6 flex-col text-center body-text pt-8 pb-4">
                 <div className="pb-6">
